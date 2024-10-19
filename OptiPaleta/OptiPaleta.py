3026a80381e454dbdc4f68fbabf0893c22c36e2a
DebugDuck-quack <184690899+DebugDuck-quack@users.noreply.github.com>
2024-10-20 00:51:33 +0200

    aktualizacja

---
+++ OptiPaleta/OptiPaleta.py
@@ -0,0 +1,9 @@
+import tkinter as tk  
+import customtkinter as ctk  
+from gui_handler import BoxOptimizerApp  
+
+
+if __name__ == "__main__":
+    root = ctk.CTk()  
+    app = BoxOptimizerApp(root)  
+    root.mainloop()  
