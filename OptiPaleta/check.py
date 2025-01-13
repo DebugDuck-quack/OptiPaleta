01e13de165d8a5f3cb3eb8f773d5e5cf0d4820a2
DebugDuck-quack <184690899+DebugDuck-quack@users.noreply.github.com>
2025-01-13 21:54:40 +0100

    Update check.py

--- OptiPaleta/check.py
+++ OptiPaleta/check.py
@@ -1,7 +1,7 @@
 import customtkinter as ctk
 from tkinter import messagebox
 
-# Klasa do walidacji danych wejściowych
+# Klasa do walidacji
 def validate_box_entry(box_entry):
     try:
         box_data = box_entry.get().split(',')
@@ -16,11 +16,11 @@ def validate_box_entry(box_entry):
         if any(d <= 0 for d in dimensions) or weight <= 0 or quantity <= 0:
             raise ValueError("Wymiary, waga i ilość muszą być dodatnie.")
         
-        # Jeśli dane są poprawne, zmień kolor pola na zielony
+        # Jeśli dane są poprawne zmieniam kolor pola na zielony
         box_entry.configure(fg_color="green")
         return True
     except ValueError:
-        # Jeśli dane są niepoprawne, zmień kolor pola na czerwony i pokaż błąd
+        # Jeśli dane są niepoprawne zmieniam kolor pola na czerwony i pokaż błąd
         messagebox.showerror("Błąd", "Wprowadź poprawne dane pudełka w formacie: Dł, Szer, Wys, Waga, Ilość, Blokada osi [opcjonalnie]")
         box_entry.configure(fg_color="red")
         return False
@@ -31,12 +31,12 @@ def validate_pallet_entry(pallet_entry, max_weight_entry):
         pallet_length, pallet_width, pallet_height = map(int, pallet_data)
         max_weight_limit = float(max_weight_entry.get())
 
-        # Jeśli dane są poprawne, zmień kolor pól na zielony
+        # Jeśli dane są poprawne zmieniam kolor pól na zielony
         pallet_entry.configure(fg_color="green")
         max_weight_entry.configure(fg_color="green")
         return True
     except ValueError:
-        # Jeśli dane są niepoprawne, zmień kolor pól na czerwony i pokaż błąd
+        # Jeśli dane są niepoprawne zmieni sie kolor pól na czerwony i pokaże błąd
         messagebox.showerror("Błąd", "Wprowadź poprawne dane palety i maksymalne obciążenie")
         pallet_entry.configure(fg_color="red")
         max_weight_entry.configure(fg_color="red")
